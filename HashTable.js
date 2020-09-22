4a6ee50bf7ba0b0394eae479f1d6deac1753aed7
prince tanwar <ptanwar468@gmail.com>
2020-09-22 07:16:06 +0530

    hash table

---
+++ HashTable.js
@@ -0,0 +1,67 @@
+class HashTable {
+  constructor(storageLimit) {
+    this.storage = new Array(storageLimit);
+    this.storageLimit = storageLimit;
+  }
+
+  hash(key, max) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash += key.charCodeAt(i);
+    }
+    return hash % max;
+  }
+
+  add(key, value) {
+    let index = this.hash(key, this.storageLimit);
+    if (this.storage[index] === undefined) {
+      this.storage[index] = [[key, value]];
+    } else {
+      let inserted = false;
+      for (let i = 0; i < this.storage[index].length; i++) {
+        if (this.storage[index][i][0] === key) {
+          this.storage[index][i][1] = value;
+          inserted = true;
+        }
+      }
+      if (inserted === false) {
+        this.storage[index].push([key, value]);
+      }
+    }
+  }
+  remove(key) {
+    let index = this.hash(key, this.storageLimit);
+    if (this.storage[index].length === 1 && this.storage[index][0][0] === key) {
+      delete this.storage[index];
+    } else {
+      for (let i = 0; i < this.storage[index].length; i++) {
+        if (this.storage[index][i][0] === key) {
+          delete this.storage[index][i];
+        }
+      }
+    }
+  }
+
+  lookup(key) {
+    let index = this.hash(key, this.storageLimit);
+    if (this.storage[index] === undefined) {
+      return undefined;
+    } else {
+      for (let i = 0; i < this.storage[index].length; i++) {
+        if (this.storage[index][i][0] === key) {
+          return this.storage[index][i][1];
+        }
+      }
+    }
+  }
+}
+
+const ht = new HashTable(4);
+ht.add("prince", 8426);
+ht.add("princ", 888);
+
+ht.add("ram", 426);
+
+console.log(ht.storage);
+
+// console.log("ans", ht.lookup("prince"));
